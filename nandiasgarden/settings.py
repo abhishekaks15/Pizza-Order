961aa28c5044e23559129ee4e5f4b1d49bafc322
abhishekaks15 <74969828+abhishekaks15@users.noreply.github.com>
2021-07-08 12:12:37 +0530

    Update settings.py

--- nandiasgarden/settings.py
+++ nandiasgarden/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h=o%y!dljkkx!--^1-=)!gb*tapu1r2e9650u)1lx1l0%^f&+z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','orderpizzahouse.herokuapp.com']
 
 
 # Application definition
